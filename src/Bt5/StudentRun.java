b8d1219e465a130e5fd75bf2153f31fa8def4177
THAOJV240603 <lethao0815@gmail.com>
2024-09-03 11:19:56 +0900

    update

--- src/Bt5/StudentRun.java
+++ src/Bt5/StudentRun.java
@@ -1,13 +1,37 @@
 package Bt5;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class StudentRun {
     public static void main(String[] args) {
-        Student student = new Student();
+        List<Student> list = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
 
-        //Nhập thông tin
-        student.inputData();
+        //In Menu
+        do{
+            System.out.println("======== MENU ========\n" + "1. Hiển thị danh sách tất cả sinh viên\n" +
+                    "2. Thêm mới sinh viên\n" +
+                    "3. Sửa thông tin sinh viên dựa vào mã sinh viên\n" +
+                    "4. Xóa sinh viên dựa vào mã sinh viên\n" +
+                    "5. Thoát");
+            System.out.println("Mời bạn chọn 1 -> 5");
+            int choice = sc.nextInt();
+            switch(choice) {
+                case 1:
+                    //Hiển thị danh sách sinh viên
+                    System.out.println("------ Danh sách sinh viên ------");
+                    for(Student student : list) {
+                        student.displayData();
+                    }
+                    break;
+                case 5:
+                    System.exit(0);
+                default:
+                    System.out.println("Sai lựa chọn");
+            }
+        }while(true);
 
-        //Hiển thị thông tin
-        student.displayData();
     }
 }
